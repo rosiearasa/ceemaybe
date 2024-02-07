fac96cdeff8497df270cb6f2e6b0846ca3dea3f5
rosiearasa <rosearasa7522@gmail.com>
2024-02-07 22:02:25 +0300

    temp data for db

--- lib/data/plunge.dart
+++ lib/data/plunge.dart
@@ -6,26 +6,35 @@ class Plunge {
   DateTime dateTimeStarted;
   DateTime dateTimeCompleted;
   int duration;
+  int temperature;
   String plungeMethod;
   String shiver;
-  
-  Plunge(this.locationLatitide, this.locationLongitude, this.dateTimeStarted, this.dateTimeCompleted, this.duration, this.plungeMethod, this.shiver);
 
-    Map<String, dynamic> toMap() {
+  Plunge(this.locationLatitide, this.locationLongitude, this.dateTimeStarted,
+      this.dateTimeCompleted, this.duration,this.temperature, this.plungeMethod, this.shiver);
+
+  Map<String, dynamic> toMap() {
     return {
       'locationLatitide': locationLatitide,
       'locationLongitude': locationLongitude,
       'dateTimeStarted': dateTimeStarted,
       'dateTimeCompleted': dateTimeCompleted,
       'duration': duration,
-      'plungeMethod' : plungeMethod,
-      'shiver' : shiver,
+      'temperature': temperature,
+      'plungeMethod': plungeMethod,
+      'shiver': shiver,
     };
   }
 
   static Plunge fromMap(Map<String, dynamic> map) {
     return Plunge(
-        map['locationLatitide'], map['locationLongitude'], map['dateTimeStarted'], map['dateTimeCompleted'], map['duration'], map['plungeMethod'], map['shiver']);
+        map['locationLatitide'],
+        map['locationLongitude'],
+        map['dateTimeStarted'],
+        map['dateTimeCompleted'],
+        map['duration'],
+        map['temperature'],
+        map['plungeMethod'],
+        map['shiver']);
   }
-
 }
